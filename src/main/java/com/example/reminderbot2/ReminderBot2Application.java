2f1d8a6de27e33aa43b51be5e41807d90c84a968
Eugene <kolchak-zha@yandex.ru>
2023-05-20 17:39:15 +0400

    Первый коммит

---
+++ src/main/java/com/example/reminderbot2/ReminderBot2Application.java
@@ -0,0 +1,13 @@
+package com.example.reminderbot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReminderBot2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReminderBot2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/reminderbot2/ReminderBot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reminderbot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReminderBot2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
